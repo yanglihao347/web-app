74f12c4823a7da040be0c390799aa8a216212554
yanglihao347 <yanglihao347@gmail.com>
2019-09-24 01:12:18 +0800

    目录搭建

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Button } from 'antd';
+
+class App extends React.Component {
+    render (){
+        return (
+            <div>
+                <Button>按钮test</Button>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
