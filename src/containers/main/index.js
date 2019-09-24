22ac3e9dc86497c45aded1f0703ff22fc070d424
yanglihao347 <yanglihao347@gmail.com>
2019-09-25 00:51:57 +0800

    修改title

--- src/containers/main/index.js
+++ src/containers/main/index.js
@@ -14,12 +14,11 @@ export default class Main extends Component {
                 <Menu
                     theme="dark"
                     mode="horizontal"
-                    defaultSelectedKeys={['2']}
+                    defaultSelectedKeys={['inventory']}
                     style={{ lineHeight: '64px' }}
                 >
-                    <Menu.Item key="1">nav 1</Menu.Item>
-                    <Menu.Item key="2">nav 2</Menu.Item>
-                    <Menu.Item key="3">nav 3</Menu.Item>
+                    <Menu.Item key="inventory">库存</Menu.Item>
+                    <Menu.Item key="order">订单</Menu.Item>
                 </Menu>
                 </Header>
                 <Layout>
