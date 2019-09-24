1ad13ebebd0081339f101374d6450e687373726d
yanglihao347 <yanglihao347@gmail.com>
2019-09-25 00:15:48 +0800

    主页面布局

---
+++ src/containers/login/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { PageHeader } from 'antd';
+
+export default class Login extends Component {
+    render () {
+        return (
+            <div>
+                <PageHeader title="登录" />
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button } from 'antd';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store'
+import Login from './containers/login';
+import Main from './containers/main';
+
 
 class App extends React.Component {
     render (){
         return (
-            <div>
-                <Button>按钮test</Button>
-            </div>
+            <Provider store={store} >
+                <HashRouter>
+                    <Switch>
+                        <Route path='/login' component={Login}></Route>
+                        <Route component={Main}></Route>
+                    </Switch>
+                </HashRouter>
+            </Provider>
         )
     }
 }
