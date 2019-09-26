bbcec164488be3196f25ab9db66dc134be1c57f8
yanglihao347 <yanglihao347@gmail.com>
2019-09-27 01:42:57 +0800

    安装MD5依赖

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import { PageHeader } from 'antd';
+import md5 from 'blueimp-md5';
 
 export default class Login extends Component {
+    
     render () {
+        console.log(md5('avnc'));
         return (
             <div>
                 <PageHeader title="登录" />
