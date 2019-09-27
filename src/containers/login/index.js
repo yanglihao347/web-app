ded9025df1af87be5c2d2e9df27ad8e84ab2fef2
wb-ylh433393 <wb-ylh433393@alibaba-inc.com>
2019-09-27 20:21:07 +0800

    研究redux

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import { PageHeader } from 'antd';
+import { PageHeader, Button } from 'antd';
 import md5 from 'blueimp-md5';
+import { reqLogin } from '../../api';
 
 export default class Login extends Component {
     
     render () {
-        console.log(md5('avnc'));
+
         return (
             <div>
                 <PageHeader title="登录" />
+                <Button onClick={() => { reqLogin({username: 'abcxx',password: '12345' }) }}>登录</Button>
             </div>
         )
     }
