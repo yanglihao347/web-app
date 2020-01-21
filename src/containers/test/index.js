b9f107e81237e709c2a591799a1957aa1bfe8748
wb-ylh433393 <wb-ylh433393@alibaba-inc.com>
2020-01-21 10:26:28 +0800

    测试结束

--- src/containers/test/index.js
+++ src/containers/test/index.js
@@ -1,11 +1,18 @@
 /* eslint-disable */
 import React, { Component } from 'react';
-import { PageHeader, Button, message } from 'antd';
+import { PageHeader, Button, message, Drawer, Tag } from 'antd';
 // import md5 from 'blueimp-md5';
 import { connect } from 'react-redux';
+import './index.scss';
+
+const { CheckableTag } = Tag;
 
 class Test extends Component {
 
+  state = {
+    visible: false,
+    checked: false,
+  }
     
   render () {
 
@@ -19,8 +26,35 @@ class Test extends Component {
         <img src="http://json.image.alimmdn.com/vsou.png" />
         <img src="https://test003-0106.oss-cn-hangzhou.aliyuncs.com/F2L_19.gif" />
         <button id="rChange">随机切换</button>
-        <button id="showBtn">显示/隐藏 公式</button>
-        <div id="formula" style="display:none">公式</div>
+        <button id="showBtn" onClick={() => this.setState({ visible: true })}>显示/隐藏 公式</button>
+        <Drawer
+          title="abc"
+          placement="left"
+          visible={this.state.visible}
+          width="700"
+          onClose={() => this.setState({
+            visible: false,
+          })}
+        >
+          <CheckableTag
+            checked={this.state.checked}
+            onChange={(checked) => {
+              this.setState({
+                checked
+              })
+            }}
+            className="aaa"
+          >
+            abc
+          </CheckableTag>
+          <Button
+            size="large"
+            type="primary"
+          >
+            随机练习
+          </Button>
+        </Drawer>
+
       </div>
     )
   }
